495505ef580ddc282acf16663cfa594f26106065
lantian <lantian.ivan@gmail.com>
2014-03-06 18:32:23 +0200

    Added phpdoc theme for API (with small changes). Generated Engine, Core, User API.

--- en/make-api.sh
+++ en/make-api.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-echo -n "Type PhalconEye app directory location, followed by [ENTER]: "
+# Find php doc directory and copy api theme there.
+echo 'Copying API theme into phpdoc directory..'
+PHPDOCLOCATION="$(pear list-files phpdoc/phpDocumentor | grep VERSION | sed -e 's/php    //g')"
+PHPDOCLOCATION="$(dirname "$PHPDOCLOCATION")/data/templates"
+$(sudo cp -fr ./_theme/phalconeye-api/* ${PHPDOCLOCATION}/phalconeye-api)
+
+# Ask about PhalconEye app dir
+echo -n "Type PhalconEye 'app' directory location, followed by [ENTER]: "
 read LOCATION
 THEME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/_theme/api/"
 
-`sudo phpdoc -d $LOCATION/engine,$LOCATION/modules -t ./api -i Package/Structure/,Asset/Css/lessc.inc.php,Behaviour/DIBehaviour.php --template=$THEME`
+# Cleanup phpdoc twig cache
+$(rm -rf /temp/phpdoc-twig-cache)
+
+# Make api docs..
+$(phpdoc -d ${LOCATION}/engine,${LOCATION}/modules -t ./api -i Package/Structure/,Asset/Css/lessc.inc.php,Behaviour/DIBehaviour.php --template "phalconeye-api")
